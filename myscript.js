79426cb0c03f2780efe55aac62698b4a76b2542c
D <142498566+davylocatelli@users.noreply.github.com>
2023-11-27 17:58:32 +0000

    a

--- myscript.js
+++ myscript.js
@@ -1,7 +1,9 @@
+/*
 function adicionar(){
     var atual = 0
     var novo = atual + 1;
     document.getElementById("quantidade").value = novo;
+    document.write(novo)
 }
 
 function menos(){
@@ -11,3 +13,27 @@ function menos(){
       document.getElementById("quantidade").value = novo;
     }
   }
+  */
+function total() {
+  var total1 = 0
+  var preço1 = document.getElementById("idPreço1").value
+  console.log(preço1)
+  var quant = document.getElementById("idQuantidade").value
+  console.log(quant)
+  total1 = preço1 * quant
+  console.log(total1)
+
+  document.getElementById("preçototal1").value = "R$" + total1
+}
+
+function total2() {
+  var total2 = 0
+  var preço2 = document.getElementById("idPreço2").value
+  console.log(preço2)
+  var quant = document.getElementById("quantidade2").value
+  console.log(quant)
+  total2 = preço2 * quant
+  console.log(total2)
+
+  document.getElementById("preçototal2").value ="R$"+total2
+}

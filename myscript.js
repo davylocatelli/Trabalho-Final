25fa6f80e065f558d097a5ab73d735e57c291f06
D <142498566+davylocatelli@users.noreply.github.com>
2023-11-27 19:46:06 +0000

    dia27

--- myscript.js
+++ myscript.js
@@ -22,7 +22,6 @@ function total() {
   console.log(quant)
   total1 = preço1 * quant
   console.log(total1)
-
   document.getElementById("preçototal1").value = "R$" + total1
 }
 
@@ -37,3 +36,28 @@ function total2() {
 
   document.getElementById("preçototal2").value ="R$"+total2
 }
+
+function total3() {
+  var total3 = 0
+  var preço3 = document.getElementById("idPreço3").value
+  console.log(preço3)
+  var quant = document.getElementById("quantidade3").value
+  console.log(quant)
+  total3 = preço3 * quant
+  console.log(total3)
+  document.getElementById("preçototal3").value ="R$"+total3
+}
+
+console.log(total2)
+
+
+/*
+function totalfinal() {
+  var totalfim = 0
+  var t1 = document.getElementById("preçototal1")
+  var t2 = document.getElementById("preçototal2")
+  var t3 = document.getElementById("preçototal3")
+  var totalfim = t1 + t2 + t3
+  console.log = totalfim
+  document.getElementById("preçototal").value ="R$"+totalfim
+}*/
